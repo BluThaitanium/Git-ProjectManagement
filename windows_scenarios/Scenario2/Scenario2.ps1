e814d7c1db7163342eb757eefc81922973f42a05
Phu Thai <phuthai450@gmail.com>
2025-09-20 12:50:55 -0400

    Scenario2 script

--- windows_scenarios/Scenario2/Scenario2.ps1
+++ windows_scenarios/Scenario2/Scenario2.ps1
@@ -2,3 +2,9 @@ New-Item "AddMe1.txt" -ItemType File
 New-Item "AddMe2.txt" -ItemType File
 New-Item "RestoreMe2.txt" -ItemType File
 "password123" | Set-Content "TestPassword.txt"
+
+$scriptPath = $MyInvocation.MyCommand.Path
+$currentContent = Get-Content -Path $scriptPath
+$newLine = "# Script last run on $(Get-Date -Format 'yyyy-MM-dd HH:mm:ss')"
+$newContent = "$currentContent`n$newLine"
+Set-Content -Path $scriptPath -Value $newContent
